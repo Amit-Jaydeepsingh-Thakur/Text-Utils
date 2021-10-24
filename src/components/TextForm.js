f6af068a6b6fc093daceb88e0b764c1f06769220
Amit Thakur <amit.thakur17@vit.edu>
2021-10-24 15:15:54 +0530

    Text Area addition for the TextUtils App

---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TextForm() {
+    return (
+        <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Enter Your Text</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    )
+}
+
+export default TextForm;
